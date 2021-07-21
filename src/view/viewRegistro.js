48d74b64694d54c9bebc09942d1a7cf38c7ca747
SofiaCanalesMoya <sofia.sbcm@gmail.com>
2021-07-21 09:18:27 -0400

    Pantalla de registro

---
+++ src/view/viewRegistro.js
@@ -0,0 +1,11 @@
+export const registro = () => {
+    const mainRegistro = document.createElement("div");
+    mainRegistro.className = "mainRegistro";
+
+    const itemImg = document.createElement("img");
+    itemImg.src = 'src/img/brocoli.png';
+    itemImg.className = "imagen";
+    mainRegistro.appendChild(itemImg);
+
+    // document.getElementById("root").appendChild(mainRegistro);
+}
