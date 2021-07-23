204ea8d8ed1d81a364e4dca8af7acca1bee7bc56
Solange Gonzalez <solgonzalezaraya@gmail.com>
2021-07-23 10:41:20 -0400

    listos estilos login para desktop

--- src/lib/view/viewLogin.js
+++ src/lib/view/viewLogin.js
@@ -3,10 +3,10 @@ import { loginGoogle } from '../loginGoogle.js';
 export const login = () => {
 
     const mainLogin = document.createElement("div");
-    mainLogin.className = "mainlogin";
+    mainLogin.className = "mainLogin";
 
     const fondoLogo = document.createElement("div");
-    fondoLogo.className = "fondologo";
+    fondoLogo.className = "fondoLogo";
     mainLogin.appendChild(fondoLogo);
 
     const itemImg = document.createElement("img");
@@ -37,6 +37,7 @@ export const login = () => {
     mailInput.placeholder = "Ingresa tu correo";
     fondoIniciarSesion.appendChild(mailInput)
 
+
     const mailPassword = document.createElement("input");
     mailPassword.className = "inputPassword";
     mailPassword.placeholder = "Ingresa tu contraseña";
@@ -47,7 +48,7 @@ export const login = () => {
     btnIniciar.className = "btnIniciar"
     btnIniciar.innerText = "Iniciar sesión"
     fondoIniciarSesion.appendChild(btnIniciar);
-
+    
     const parrafoGoogle = document.createElement("p");
     parrafoGoogle.className = "parrafoGoogle";
     const textoGoogle = document.createTextNode("O entra con");
@@ -78,12 +79,31 @@ export const login = () => {
     divRegistro.appendChild(parrafoRegistroAqui);
     fondoIniciarSesion.appendChild(divRegistro);
 
-    // document.getElementById("root").appendChild(mainLogin);
+   
+    //funcion del boton iniciar sesión
+    btnIniciar.addEventListener('click', ()=>{
+       const email = document.querySelector('.inputMail').value;
+       const password = document.querySelector('.inputPassword').value;
 
+       firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+    // ...
+        })
+        .catch((error) => {
+         var errorCode = error.code;
+        var errorMessage = error.message;
+        });
+       
+    
+    });
+
+    
+    //funcion del boton iniciar sesión google
     const btnGoogle = mainLogin.querySelector(".googleBoton");
     btnGoogle.addEventListener("click", () => {
         loginGoogle();
-    console.log("boton google");
         });
         
     return mainLogin;
--- src/lib/view/viewRegistro.js
+++ src/lib/view/viewRegistro.js
@@ -1,3 +1,5 @@
+
+
 export const registro = () => {
     const mainRegistro = document.createElement("div");
     mainRegistro.className = "mainRegistro";
@@ -66,6 +68,25 @@ export const registro = () => {
     btnRegistro.className = "btnRegistro"
     btnRegistro.innerText = "Registrarme"
     mainRegistro.appendChild(btnRegistro);
+
+    btnRegistro.addEventListener('click', ()=>{
+        const email = document.querySelector('.inputCorreo').value;
+        const password = document.querySelector('.inputPassword').value;
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          // Signed in
+        var user = userCredential.user;
+           // ...
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+              // ..
+            });
+          
+          
+    });
 
     return mainRegistro;
 }
