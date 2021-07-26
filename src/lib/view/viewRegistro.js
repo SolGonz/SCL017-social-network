5299ede177d7a32ed456e8767a5ce0290d49490d
Vania Venegas <venegas.vania@gmail.com>
2021-07-26 08:22:47 -0400

    cambios en registro

--- src/lib/view/viewRegistro.js
+++ src/lib/view/viewRegistro.js
@@ -2,48 +2,57 @@ export const registro = () => {
     const mainRegistro = document.createElement("div");
     mainRegistro.className = "mainRegistro";
 
-    const itemImagen = document.createElement("img");
-    itemImagen.src = '/img/brocoli.png';
-    itemImagen.className = "itemImagen";
-    mainRegistro.appendChild(itemImagen);
+    const fondoLogoRegistro = document.createElement("div");
+    fondoLogoRegistro.className = "fondoLogoRegistro";
+    mainRegistro.appendChild(fondoLogoRegistro);
+
+    const itemImg = document.createElement("img");
+    itemImg.src ='img/brocoli.png';
+    itemImg.className = "imagen";
+    fondoLogoRegistro.appendChild(itemImg);
 
     const tituloRegistro = document.createElement("div");
     tituloRegistro.className = "tituloRegistro";
     const textoTitulo = document.createTextNode("Regístrate");
     tituloRegistro.appendChild(textoTitulo);
-    mainRegistro.appendChild(tituloRegistro);
+    fondoLogoRegistro.appendChild(tituloRegistro);
 
-    const datos = document.createElement("div");
-    datos.className = "datos";
-        const textoNombre = document.createTextNode("Nombre");
-        datos.appendChild(textoNombre);
-        textoNombre.className = "textoNombre";
-            const inputNombre = document.createElement("input");
-            inputNombre.className = "inputNombre";
-            inputNombre.placeholder = "Ingresa tu nombre";
-            datos.appendChild(inputNombre)
-        const textoCorreo = document.createTextNode("Correo electrónico");
-        datos.appendChild(textoCorreo);
-        textoCorreo.className = "textoCorreo";
-            const inputCorreo = document.createElement("input");
-            inputCorreo.className = "inputCorreo";
-            inputCorreo.placeholder = "Ingresa tu correo";
-            datos.appendChild(inputCorreo);
-        const textoContraseña = document.createTextNode("Contraseña");
-        datos.appendChild(textoContraseña);
-        textoCorreo.className = "textoContraseña";
-            const mailPassword = document.createElement("input");
-            mailPassword.className = "inputPassword";
-            mailPassword.placeholder = "Ingresa tu contraseña";
-            mailPassword.type = "password"
-            datos.appendChild(mailPassword);
-        const textoDieta = document.createTextNode("¿Qué dieta sigues?");
-        datos.appendChild(textoDieta);
-        textoDieta.className = "textoDieta";
-            const selectDieta = document.createElement("select");
-            selectDieta.className = "selectDieta";
-            selectDieta.id= "selectD";
-            const optionCero = document.createElement("option");
+    const fondoRegistrarse = document.createElement("div");
+    fondoRegistrarse.className = "fondoRegistrarse";
+    mainRegistro.appendChild(fondoRegistrarse);
+
+  /*  const datos = document.createElement("div");
+    datos.className = "datos";*/
+
+    const textoNombre = document.createTextNode("Nombre");
+    fondoRegistrarse.appendChild(textoNombre);
+    textoNombre.className = "textoNombre";
+    const inputNombre = document.createElement("input");
+    inputNombre.className = "inputNombre";
+    inputNombre.placeholder = "Ingresa tu nombre";
+    fondoRegistrarse.appendChild(inputNombre)
+    const textoCorreo = document.createTextNode("Correo electrónico");
+    fondoRegistrarse.appendChild(textoCorreo);
+    textoCorreo.className = "textoCorreo";
+    const inputCorreo = document.createElement("input");
+    inputCorreo.className = "inputCorreo";
+    inputCorreo.placeholder = "Ingresa tu correo";
+    fondoRegistrarse.appendChild(inputCorreo);
+    const textoContraseña = document.createTextNode("Contraseña");
+    fondoRegistrarse.appendChild(textoContraseña);
+    textoCorreo.className = "textoContraseña";
+    const mailPassword = document.createElement("input");
+    mailPassword.className = "inputPassword";
+    mailPassword.placeholder = "Ingresa tu contraseña";
+    mailPassword.type = "password"
+    fondoRegistrarse.appendChild(mailPassword);
+    const textoDieta = document.createTextNode("¿Qué dieta sigues?");
+    fondoRegistrarse.appendChild(textoDieta);
+    textoDieta.className = "textoDieta";
+    const selectDieta = document.createElement("select");
+    selectDieta.className = "selectDieta";
+    selectDieta.id= "selectD";
+    const optionCero = document.createElement("option");
             optionCero.value = 'Dieta';
             optionCero.text = "Dieta";
             const optionUno = document.createElement("option");
@@ -59,13 +68,13 @@ export const registro = () => {
             selectDieta.appendChild(optionUno);
             selectDieta.appendChild(optionDos);
             selectDieta.appendChild(optionTres);
-         datos.appendChild(selectDieta);
-    mainRegistro.appendChild(datos);
+            fondoRegistrarse.appendChild(selectDieta);
+    mainRegistro.appendChild(fondoRegistrarse);
 
     const btnRegistro = document.createElement("button");
     btnRegistro.className = "btnRegistro"
     btnRegistro.innerText = "Registrarme"
-    mainRegistro.appendChild(btnRegistro);
+    fondoRegistrarse.appendChild(btnRegistro);
 
     return mainRegistro;
 }
