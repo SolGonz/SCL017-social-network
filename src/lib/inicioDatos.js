ea7cac4682b149797adff264866c542710cfdc11
Sofía Canales Moya <83974652+SofiaCanalesMoya@users.noreply.github.com>
2021-08-17 07:56:18 -0400

    Feature rama sofia (#11)
    * m
    
    * m
    
    * button editar creado

---
+++ src/lib/inicioDatos.js
@@ -0,0 +1,10 @@
+
+// Your web app's Firebase configuration
+export var firebaseConfig = {
+    apiKey: "AIzaSyBR5ytaN8RV09i9qV4b4gxUj720zy2q6Kg",
+    authDomain: "veggiebookrecipes.firebaseapp.com",
+    projectId: "veggiebookrecipes",
+    storageBucket: "veggiebookrecipes.appspot.com",
+    messagingSenderId: "926850678058",
+    appId: "1:926850678058:web:467ea05b2de5d1b3ba7253"
+};
